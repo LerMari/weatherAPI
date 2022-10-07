79c554677b9be3aa0c1994a27e7b42cd94dcd669
Marisa <lermamarisa@gmail.com>
2022-10-06 20:42:16 -0700

    added google fonts

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
 //alert("connected");
+//const url="https://api.openweathermap.org/data/2.5/weather?appid=2085d3c9972fa661e949980abea9ea10&units=imperial&zip=94040";
+//fetch(url)
+//.then(response => response.json())
+//.then(data => {
+    //console.log(data.name);
+    //console.log(data.main.temp);
+  //  console.log(data.weather[0].description);
+//})
 
 document.getElementById("btn").addEventListener("click", getForecast);
 
 const results = document.getElementById("results");
 
-const url="https://api.openweathermap.org/data/2.5/weather?appid=2085d3c9972fa661e949980abea9ea10&units=imperial&zip=94040";
-
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    console.log(data.name);
-    console.log(data.main.temp);
-    console.log(data.weather[0].description);
-})
-
 function getForecast() {
     console.log("clicked");
     const endpoint = "https://api.openweathermap.org/data/2.5/weather?appid=2085d3c9972fa661e949980abea9ea10&units=imperial&zip=";
-    
     let zipcode = document.getElementById("zipcode").value ;
-
     console.log("zipcode is " + zipcode);
-
     let newUrl = `${endpoint}${zipcode}`;
-
     console.log(newUrl);
-
     fetch(newUrl)
     .then(response => response.json())
     .then(data => {
